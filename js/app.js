21c6d0b17c8bcb6a4cca91a09d32c1533d2c642f
Abhiraj <abhiraj4u@hotmail.com>
2016-06-03 22:26:52 +0530

    Added Parameters functionality

--- js/app.js
+++ js/app.js
@@ -1,48 +1,36 @@
-app.run(function($rootScope) {
-	angular.element(document).on("click", function(e) {
-		$rootScope.$broadcast("documentClicked", angular.element(e.target));
-	});
+var restApp = angular.module('restApp', []);
+
+restApp.controller('Ctrl', function($scope) {
+    
+    $scope.restMethod = [
+        {req : "GET"},
+        {req : "POST"},
+        {req : "PUT"},
+        {req : "DELETE"}
+    ];
+    
+    $scope.url = "Enter URL Here";
+    
+    var counter = 0;
+    $scope.param = {
+    fields: []
+    }
+    $scope.days = ['Day', 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    
+    $scope.addParams = function(){
+        $scope.param.fields.push({
+            name: "Param" + counter++
+        });
+    };
+    
 });
 
-app.directive("dropdown", function($rootScope) {
-	return {
-		restrict: "E",
-		templateUrl: "templates/dropdown.html",
-		scope: {
-			placeholder: "@",
-			list: "=",
-			selected: "=",
-			property: "@"
-		},
-		link: function(scope) {
-			scope.listVisible = false;
-			scope.isPlaceholder = true;
-
-			scope.select = function(item) {
-				scope.isPlaceholder = false;
-				scope.selected = item;
-			};
-
-			scope.isSelected = function(item) {
-				return item[scope.property] === scope.selected[scope.property];
-			};
-
-			scope.show = function() {
-				scope.listVisible = true;
-			};
-
-			$rootScope.$on("documentClicked", function(inner, target) {
-				console.log($(target[0]).is(".dropdown-display.clicked") || $(target[0]).parents(".dropdown-display.clicked").length > 0);
-				if (!$(target[0]).is(".dropdown-display.clicked") && !$(target[0]).parents(".dropdown-display.clicked").length > 0)
-					scope.$apply(function() {
-						scope.listVisible = false;
-					});
-			});
-
-			scope.$watch("selected", function(value) {
-				scope.isPlaceholder = scope.selected[scope.property] === undefined;
-				scope.display = scope.selected[scope.property];
-			});
-		}
-	}
-});
+restApp.directive('demoDisplay', function($compile) {
+  return {
+    scope: {
+      demoDisplay: "=", //import referenced model to our directives scope
+      demoDays: "="
+    },
+    template: "<input ng-model = 'demoDisplay.key'><input ng-model = 'demoDisplay.value'>"
+  }
+})
