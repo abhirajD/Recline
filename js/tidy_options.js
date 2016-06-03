e18095376abbae26e2afdea337c35366665ee25f
Abhiraj <abhiraj4u@hotmail.com>
2016-06-04 01:38:11 +0530

    Fixed previous imcomplete commit, added tidy_HTML

---
+++ js/tidy_options.js
@@ -0,0 +1,17 @@
+options = {
+  "indent":"auto",
+  "indent-spaces":4,
+  "wrap":80,
+  "markup":true,
+  "output-xml":false,
+  "numeric-entities":true,
+  "quote-marks":true,
+  "quote-nbsp":false,
+  "show-body-only":true,
+  "quote-ampersand":false,
+  "break-before-br":true,
+  "uppercase-tags":false,
+  "uppercase-attributes":false,
+  "drop-font-tags":true,
+  "tidy-mark":false
+}
