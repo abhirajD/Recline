0425f035dabd3c95eee49dab6a05c8080a8600ec
Abhiraj <abhiraj4u@hotmail.com>
2016-06-03 23:34:42 +0530

    URL Processing done

--- js/app.js
+++ js/app.js
@@ -10,18 +10,45 @@ restApp.controller('Ctrl', function($scope) {
     ];
     
     $scope.url = "Enter URL Here";
+    $scope.finalUrl = "";
     
     var counter = 0;
     $scope.param = {
     fields: []
     }
+    
     $scope.addParams = function(){
         $scope.param.fields.push({
             name: "Param" + counter++
         });
     };
     
+    
+    $scope.makeUrl = function() {
+        $scope.urlParams = '';
+    
+        angular.forEach($scope.param.fields, function(item) {
+            if(item.key)
+            {
+                if(item.value)
+                {
+                    $scope.urlParams += "&" + item.key + '=' + item.value;
+                }
+            };
+        });
+        
+        if($scope.urlParams)
+            $scope.finalUrl = $scope.url +'?' + $scope.urlParams.substring(1);
+            return $scope.finalUrl;
+        else
+            $scope.finalUrl = $scope.url;
+            return $scope.finalUrl;
+    };
+    
+    
+    
 });
+
 
 restApp.directive('demoDisplay', function($compile) {
   return {
@@ -29,6 +56,6 @@ restApp.directive('demoDisplay', function($compile) {
       demoDisplay: "=", //import referenced model to our directives scope
       demoDays: "="
     },
-    template: "<input ng-model = 'demoDisplay.key'><input ng-model = 'demoDisplay.value'>"
+    template: "<input ng-model = 'demoDisplay.key' ng-click = 'addParams()'><input ng-model = 'demoDisplay.value'>"
   }
 })
